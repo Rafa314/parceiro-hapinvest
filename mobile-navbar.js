aef23c134b121a368f7850e0a52ba9090cf7a4c3
unknown <rei42782@gmail.com>
2023-06-27 21:43:12 -0300

    vaii

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,5 +1,5 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 600)
+    header.classList.toggle('rolagem', window.scrollY > 300)
 })
